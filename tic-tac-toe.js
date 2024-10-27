32b910e5288d8fb780505aaf67b89e94f29b288f
Albert Thomas <aathomasdev@gmail.com>
2024-10-27 18:14:00 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const state = ["", "", "", "", "", "", "", "", ""];
+    state = ["", "", "", "", "", "", "", "", ""];
     current_val = "X";
   
 
     const win_com = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
+    
     function game() {
-        const box = document.querySelectorAll("#board > div");
+        box = document.querySelectorAll("#board > div");
         box.forEach((box,index) => {
             box.setAttribute("class", "square");
 
@@ -19,6 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 box.classList.remove('hover');
                 
             });
+
+            
 
             box.addEventListener('click', function () {
             
@@ -50,23 +52,30 @@ document.addEventListener('DOMContentLoaded', function() {
                             if(state[win_com[i][0]] == state[win_com[i][1]] && state[win_com[i][0]] == state[win_com[i][2]] && state[win_com[i][1]] == state[win_com[i][2]]){
                                 // console.log("Winner detected");
                                 // console.log("Congratulations! " + state[win_com[i][0]] + " is the Winner!");
-                                const winner_msg = document.getElementById('status');
+                                winner_msg = document.getElementById('status');
                                 winner_msg.innerHTML = "Congratulations! " + state[win_com[i][0]] + " is the Winner!";
                                 winner_msg.classList.add("#status","you-won");
                                 break;
                             }
                         }
-                        
-                        
+                     
                     }
+                    
+                    
 
                         
                 }
+            
             });
         });
         // document.getElementById("board").setAttribute("class", "square");
         
-    
+        newgame = document.querySelector("button");
+        newgame.addEventListener('click', function() {
+            location.reload();
+            // console.log("newgame button pressed");
+                                
+        });
         
     }
     game();
