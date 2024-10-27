ca77f09a272d88c47656d22c95f401f9c607d415
Albert Thomas <aathomasdev@gmail.com>
2024-10-27 04:35:33 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const state = ["", "", "", "", "", "", "", "", ""];
+    current_val = "X"
+
     function game() {
         const box = document.querySelectorAll("#board > div");
-        box.forEach(box => {
+        box.forEach((box,index) => {
             box.setAttribute("class", "square");
+
+            box.addEventListener('click', function () {
+            
+                if (!state[index]) {
+                    
+                    
+
+                    box.innerHTML = current_val;
+                    state[index] = current_val;
+                    
+                    if(current_val == "X"){
+                        box.classList.add("square", "X");
+                    }else{
+                        box.classList.add("square", "O");
+                    }
+                            
+                    console.log("State:", state);
+
+                    if (current_val == "X"){
+                        current_val = "O"
+                    }else{
+                        current_val = "X"
+                    }
+                        
+                }
+            });
         });
         // document.getElementById("board").setAttribute("class", "square");
+        
         
     }
     game();
